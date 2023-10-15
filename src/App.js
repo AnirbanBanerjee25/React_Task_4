6d02ecda37d63dd8668636adae7d9310de2ca6ba
Anirban Banerjee <anirban.banerjee2021@gmail.com>
2023-10-15 21:44:18 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      backgroundColor: 'white',
+      showColors: false,
+    };
+  }
+
+  pickColors = () => {
+    this.setState({
+      showColors: true,
+    });
+  };
+
+  selectColor = (color) => {
+    this.setState({
+      backgroundColor: color,
+      showColors: false,
+    });
+  };
+
+  render() {
+    const colors = ['red', 'yellow', 'green', 'blue', 'orange', 'purple', 'pink', 'brown', 'teal', 'lavender', 'cyan', 'gray', 'maroon', 'indigo', 'turquoise', 'coral', 'olive', 'peach','magenta', 'salmon', 'beige', 'silver', 'gold'];
+
+    return (
+      <div className="App" style={{ backgroundColor: this.state.backgroundColor }}>
+        <h1>PICK A COLOUR TO SEE THE CHANGE!</h1>
+        <button className="pick-button" onClick={this.pickColors}>
+          Pick a colour
+        </button>
+        {this.state.showColors && (
+          <div className="color-row">
+            {colors.map((color, index) => (
+              <div
+                key={index}
+                className="color"
+                style={{ backgroundColor: color }}
+                onClick={() => this.selectColor(color)}
+              />
+            ))}
+          </div>
+        )}
+      </div>
+    );
+  }
+}
+
+export default App;
